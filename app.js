407877b13c160f04c19bad7024f98d600dbbf262
shripriyaprabhakar <37489016+shripriyaprabhakar@users.noreply.github.com>
2018-06-01 12:35:37 -0700

    added code to body. need to refine

--- app.js
+++ app.js
@@ -10,14 +10,31 @@ $(document).ready(function() {
      $('.textField').val('');
 });
 
-// $('.textField').on('keyup' , function(){
-//   let textFieldValue = $('.textField').val();
-//   $('.debug').text(textFieldValue);
-// });
   $(".getData").on('click', function() {
     let retrievedData = localStorage.getItem('myFormTextData');
      $('.debug').text(retrievedData);
   });
   
+ var funcIsSorted = ( function isSorted(arr) {
+  let direction = -(arr[0] - arr[1]);
+  for (let [i, val] of arr.entries()) {
+    direction = !direction ? -(arr[i - 1] - arr[i]) : direction;
+    if (i === arr.length - 1) return !direction ? 0 : direction;
+    else if ((val - arr[i + 1]) * direction > 0) return 0;
+  }
+}).toString();
+
+
+   $(".addcode").on('click', function() {
+    // let textAreaValue = $('.textArea').val();
+    // $('textAreaValue').append("<div>isSorted</div>");
+    // $('.codeContainer').text(textAreaValue);
+    // let snippetIsSorted = localStorage.getItem('textAreaValue');
+      var $textAreaValue= $('<div class="codeContainer">' + funcIsSorted + '</div>');
+        $('body').append($textAreaValue);
+  });
+ 
+
+
 });
 
